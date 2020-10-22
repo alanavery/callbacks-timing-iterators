81778421cbe28362813272db64912841926d7a6b
delayedaa <delayedaa@gmail.com>
2020-10-22 09:10:47 -0500

    [check-number.js] Repeat checkNumber function five times

---
+++ check-number.js
@@ -0,0 +1,59 @@
+function checkNumber(num) {
+  if (num > 0) {
+    return 'positive';
+  } else if (num < 0) {
+    return 'negative';
+  } else {
+    return 'zero';
+  }
+}
+
+console.log(checkNumber(5));
+
+function checkNumber(num) {
+  if (num > 0) {
+    return 'positive';
+  } else if (num < 0) {
+    return 'negative';
+  } else {
+    return 'zero';
+  }
+}
+
+console.log(checkNumber(-10));
+
+function checkNumber(num) {
+  if (num > 0) {
+    return 'positive';
+  } else if (num < 0) {
+    return 'negative';
+  } else {
+    return 'zero';
+  }
+}
+
+console.log(checkNumber(0));
+
+function checkNumber(num) {
+  if (num > 0) {
+    return 'positive';
+  } else if (num < 0) {
+    return 'negative';
+  } else {
+    return 'zero';
+  }
+}
+
+console.log(checkNumber(101));
+
+function checkNumber(num) {
+  if (num > 0) {
+    return 'positive';
+  } else if (num < 0) {
+    return 'negative';
+  } else {
+    return 'zero';
+  }
+}
+
+console.log(checkNumber(-44));
