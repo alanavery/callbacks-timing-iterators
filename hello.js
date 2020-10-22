6e57d00b734897da052330d65b5be638fb1b6bc7
delayedaa <delayedaa@gmail.com>
2020-10-22 08:52:56 -0500

    [hello.js] Repeat sayHello function five times

---
+++ hello.js
@@ -0,0 +1,34 @@
+function sayHello(name) {
+  let msg = 'Hello, ' + name + '. How are you?';
+  return msg;
+}
+
+console.log(sayHello('SEI 2019'));
+
+function sayHello(name) {
+  let msg = 'Hello, ' + name + '. How are you?';
+  return msg;
+}
+
+console.log(sayHello('Alan Avery'));
+
+function sayHello(name) {
+  let msg = 'Hello, ' + name + '. How are you?';
+  return msg;
+}
+
+console.log(sayHello('Mickey Mouse'));
+
+function sayHello(name) {
+  let msg = 'Hello, ' + name + '. How are you?';
+  return msg;
+}
+
+console.log(sayHello('Voldemort'));
+
+function sayHello(name) {
+  let msg = 'Hello, ' + name + '. How are you?';
+  return msg;
+}
+
+console.log(sayHello('Larry, Curly and Moe'));
